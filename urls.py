746550d137a79d00c11f6a6778d7912b10f9dff8
Alex Buchanan <abuchanan@mozilla.com>
2010-06-23 17:16:39 -0700

    initial bare bones

---
+++ urls.py
@@ -0,0 +1,17 @@
+from django.conf import settings
+from django.conf.urls.defaults import *
+
+from basket import admin
+
+
+urlpatterns = patterns('',
+    #('^subscriptions/', include('subscriptions.urls')),
+)
+
+if settings.DEBUG:
+    # Remove leading and trailing slashes so the regex matches.
+    media_url = settings.MEDIA_URL.lstrip('/').rstrip('/')
+    urlpatterns += patterns('',
+        (r'^%s/(?P<path>.*)$' % media_url, 'django.views.static.serve',
+         {'document_root': settings.MEDIA_ROOT}),
+    )
